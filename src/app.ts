b22470fc559e8967c955870736abe3850e28a2c3
Quenede Abreu <quenede.in@gmail.com>
2024-01-22 11:38:42 -0500

    add file vercel

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'))
 //middleware
 app.all('*',requestIntercepter)
 
