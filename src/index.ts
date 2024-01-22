7fd8d2094664ffebfadc1019738d5b0a090c5a44
Quenede Abreu <quenede.in@gmail.com>
2024-01-22 17:23:40 -0500

    buil from aws

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,8 @@
-// import app from './app';
+import app from './app';
 import 'dotenv/config';
 import https from 'https';
 import http from 'http';
-
-import express from 'express';
-import cors from 'cors';
-import siteRouters from './routers/site.routers';
-import adminRouters from './routers/admin.routers';
-import {requestIntercepter} from './utils/middleware/requestIntercepter';
-
-
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-//middleware
-app.all('*',requestIntercepter)
-
-app.use('/api',siteRouters)
-app.use('/api/admin',adminRouters)
-
+import fs from 'fs';
 
 const runServer = (port:number, server: http.Server) =>{
   server.listen(port, () => {
@@ -39,9 +19,14 @@ const runServer = (port:number, server: http.Server) =>{
 
 const regularServer = http.createServer(app);
 if(process.env.NODE_ENV === 'production'){
-    //Configurar SSL
-    //Rodar server na 80 e na 443
-    runServer(3000, regularServer);
+  const options ={
+    key:fs.readdirSync(process.env.SSL_KEY as string),
+    cert:fs.readFileSync(process.env.SSL_CERT as string)
+  }
+  const secServer = https.createServer(options, app);
+  runServer(80, regularServer);
+  runServer(443, secServer);
+        
 }else{
   const serverPort:number = process.env.PORT ? parseInt(process.env.PORT):9000;
   runServer(serverPort, regularServer);
