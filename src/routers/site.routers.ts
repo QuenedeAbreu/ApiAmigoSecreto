76deea37010d3c737769aa2c7f702b09e1875579
Quenede Abreu <quenede.in@gmail.com>
2024-01-23 15:38:17 -0500

    add libary ejs

--- src/routers/site.routers.ts
+++ src/routers/site.routers.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express';
+const path = require('path');
 import * as eventController from '../controllers/controller.events';
 import * as peopleController from '../controllers/controller.people';
 const router = Router();
 
 
 router.get('/', (req, res) => {
-    res.sendFile('home');
+    
+    res.sendFile(path.join(__dirname, '../views/home.html'));
 
 })
 router.get('/events/:id',eventController.eventsGetById);
