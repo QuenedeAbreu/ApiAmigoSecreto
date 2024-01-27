6d6cea30bf26b917ccf7ef989dfde38894816979
Quenede Abreu <quenede.in@gmail.com>
2024-01-27 14:28:11 -0500

    Add model and router user

--- prisma/migrations/20240127192740_add_model_user/migration.sql
+++ prisma/migrations/20240127192740_add_model_user/migration.sql
@@ -4,6 +4,8 @@ CREATE TABLE `User` (
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
