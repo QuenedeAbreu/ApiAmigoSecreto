b376db6bbbb87402f1ce1392d690284af454fa31
Quenede Abreu <quenede.in@gmail.com>
2024-08-31 02:21:22 -0500

    *

--- src/controllers/controller.deploy.ts
+++ src/controllers/controller.deploy.ts
@@ -3,7 +3,6 @@ import 'dotenv/config';
 
 export const deploy:RequestHandler = (req,res)=>{
   const tokendeploy = req.params.tokendeploy;
- // http://168.75.100.166:3000/api/box/deploy/4983edd12231c72823d7f54948c8903f62a0ea79ea99c351
  try {
   fetch(process.env.ROUTER_DEPLOY  as string + tokendeploy, {
     method: 'GET',
