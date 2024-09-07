75a277b97f1f6ef387a79cf0e2a19ce82d9e34fe
Quenede Abreu <quenede.in@gmail.com>
2024-09-06 21:45:10 -0500

    final feature vote

--- src/services/service.namekid.ts
+++ src/services/service.namekid.ts
@@ -3,7 +3,7 @@ import { PrismaClient,Prisma} from "@prisma/client"
 const prisma = new PrismaClient()
 
 //Buscar todos os nomes de crianças
-export const getAllNameKid = async () =>{
+export const getAllNameKidId = async (id_user:number) =>{
   try {
     // 1. Obter todos os NameKid
 const nameKids = await prisma.nameKid.findMany({
@@ -28,11 +28,13 @@ const nameKidsWithVoteCounts = await Promise.all(
 
     const positiveVoteCount = voteCounts.find(v => v.vote === true)?._count._all || 0;
     const negativeVoteCount = voteCounts.find(v => v.vote === false)?._count._all || 0;
-
+    const is_votede = nameKid.VoteNameKid.find(vote => vote.id_user === id_user);
+    
     return {
       ...nameKid,
       positiveVoteCount,
       negativeVoteCount,
+      is_voted: is_votede ? is_votede.vote : null,
     };
   })
 );
@@ -50,6 +52,18 @@ export const getNameKidById = async (id:number) =>{
       where:{id}, 
       include: {
       User: true,
+    },})
+  } catch (error) {
+    return false
+  }
+}
+// Buscar nome de criança por id
+export const getAllNameKid = async () =>{
+  try {
+    return await prisma.nameKid.findMany({
+      include: {
+      User: true,
+      VoteNameKid: true,
     },})
   } catch (error) {
     return false
@@ -84,6 +98,33 @@ export const deleteNameKid = async (id:number) =>{
   try {
     return await prisma.nameKid.delete({where:{id}})
   } catch (error) {
+    return false
+  }
+}
+
+//Vote namekid
+export const voteNameKid = async (id_name:number, id_user:number, vote:boolean) =>{
+  try {
+    return await prisma.voteNameKid.create({data:{id_name, id_user, vote}})
+  } catch (error) {
+    console.log(error);
+    return false
+  }
+}
+export const getVoteNameKid = async (id_name:number, id_user:number) =>{
+  try {
+    return await prisma.voteNameKid.findFirst({where:{id_name, id_user}})
+  } catch (error) {
+    console.log(error);
+    return false
+  }
+}
+//Update vote namekid
+export const updateVoteNameKid = async (id:number, vote:boolean) =>{
+  try {
+    return await prisma.voteNameKid.update({where:{id}, data:{vote}})
+  } catch (error) {
+    console.log(error);
     return false
   }
 }
